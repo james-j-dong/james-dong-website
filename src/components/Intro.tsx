065dcad9a9a31f00223709caa36879b66f9512e9
James Dong <jamesjd1024@gmail.com>
2025-02-07 13:02:30 -0600

    home page intro work

---
+++ src/components/Intro.tsx
@@ -0,0 +1,12 @@
+export default function Intro() {
+    return (
+    <>
+        <h1 className="text-3xl font-bold text-brown-700 md:text-4xl lg:text-5xl ">
+            Hey there! My name is <br/> James Dong
+        </h1>
+        <p className="mt-4 text-lg text-brown-500 md:text-xl">
+            Welcome to my website! I&apos;m a passionate developer with a love for creating fully functional web applications.
+        </p>
+      </>
+    );
+}
